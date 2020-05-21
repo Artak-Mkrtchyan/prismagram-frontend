0aca1057d79e6e94e6b386ae90ccb38202adb3d9
Artak <artak6297@gmail.com>
2020-05-22 00:24:20 +0400

    feat(Routes): added routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
+import Router from "./Router";
 import Theme from "../Styles/Theme";
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
-    Hello
+    <>
+      <GlobalStyles />
+      <Router isLoggedIn={false} />
+    </>
   </ThemeProvider>
 );
