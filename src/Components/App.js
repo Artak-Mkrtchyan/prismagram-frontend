b49bd97e7e7f79e3ccc69fa03cdb44c60fa9db28
Artak <artak6297@gmail.com>
2020-05-22 01:16:50 +0400

    feat(Apollo): connect with backend

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
+
 import GlobalStyles from "../Styles/GlobalStyles";
 import Router from "./Router";
 import Theme from "../Styles/Theme";
 
 export default () => (
   <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <Router isLoggedIn={false} />
-    </>
+    <GlobalStyles />
+    <Router isLoggedIn={false} />
   </ThemeProvider>
 );
