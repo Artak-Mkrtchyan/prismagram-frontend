64cddc3867253c8b8cefb9800eefcdcc17a1de54
Artak <artak6297@gmail.com>
2020-05-21 21:09:47 +0400

    feat(Theme): created Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,17 @@
+const BORDER_RADIUS = "4px";
+const BOX_BORDER = "1px solid #e6e6e6";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#3897f0",
+  darkGrayColor: "#999",
+  darkBlueColor: "#003569",
+  lightGrayColor: "#c7c7c7",
+  redColor: "#ED4956",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BORDER_RADIUS};
+    ${BOX_BORDER};
+    background-color: white;`,
+};
