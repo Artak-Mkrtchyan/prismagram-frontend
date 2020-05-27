fe7bedb2cd1f557ff75dff33bfb6e24c21a16b86
Artak <artak6297@gmail.com>
2020-05-28 02:02:27 +0400

    feat(Post): added styles for Post and added Helmet

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -3,19 +3,44 @@ import PropTypes from "prop-types";
 import useInput from "../../Hooks/useInput";
 import PostPresenter from "./PostPresenter";
 
-const PostContainer = ({ id, user, files, likeCount, isLiked, comments, createdAt }) => {
-  return <PostPresenter />;
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt,
+  caption,
+  location,
+}) => {
+  const [isLikedS, setIsLiked] = useState(isLiked);
+  const [likeCountS, setLikeCount] = useState(likeCount);
+  const comment = useInput("");
+  return (
+    <PostPresenter
+      user={user}
+      files={files}
+      likeCount={likeCountS}
+      isLiked={isLikedS}
+      comments={comments}
+      createdAt={createdAt}
+      newComment={comment}
+      setIsLiked={setIsLiked}
+      setLikeCount={setLikeCount}
+      caption={caption}
+      location={location}
+    />
+  );
 };
 
 PostContainer.propTypes = {
   id: PropTypes.string.isRequired,
-  user: PropTypes.objectOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      avatar: PropTypes.string,
-      username: PropTypes.string.isRequired,
-    })
-  ).isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }).isRequired,
   files: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -24,17 +49,19 @@ PostContainer.propTypes = {
   ).isRequired,
   likeCount: PropTypes.number.isRequired,
   isLiked: PropTypes.bool.isRequired,
-  comments: PropTypes.objectOf(
+  comments: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       user: PropTypes.shape({
         id: PropTypes.string.isRequired,
         username: PropTypes.string.isRequired,
-      }),
+      }).isRequired,
     })
   ).isRequired,
   createdAt: PropTypes.string.isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
 };
 
 export default PostContainer;
--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -1,1 +1,90 @@
-export default () => "Post";
+import React from "react";
+import styled from "styled-components";
+
+import FatText from "../FatText";
+import Avatar from "../Avatar";
+import { HeartFull, HeartEmpty, Comment } from "../Icons";
+
+const Post = styled.div`
+  ${(props) => props.theme.whiteBox};
+  width: 100%;
+  max-width: 600px;
+  margin-bottom: 25px;
+`;
+
+const Header = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const UserColumn = styled.div`
+  margin-left: 10px;
+`;
+
+const Location = styled.div`
+  display: block;
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+const Files = styled.div``;
+
+const File = styled.img`
+  max-width: 100%;
+`;
+
+const Button = styled.span`
+  cursor: pointer;
+`;
+
+const Buttons = styled.div`
+  ${Button} {
+    &:first-child {
+      margin-right: 10px;
+    }
+  }
+  margin-bottom: 10px;
+`;
+
+const Meta = styled.div`
+  padding: 15px;
+`;
+
+const Timestamp = styled.span`
+  font-weight: 400;
+  text-transform: uppercase;
+  opacity: 0.5;
+  font-size: 12px;
+  margin: 10px 0px;
+  padding-bottom: 10px;
+  display: block;
+  border-bottom: 1px solid ${(props) => props.theme.lightGrayColor};
+`;
+
+export default ({ user: { username, avatar }, location, files, isLiked, likeCount, createdAt }) => {
+  return (
+    <Post>
+      <Header>
+        <Avatar size='sm' url={avatar} />
+        <UserColumn>
+          <FatText text={username} />
+          <Location>{location}</Location>
+        </UserColumn>
+      </Header>
+      <Files>
+        {files && files.map((file) => <File key={file.id} src={file.url} id={file.id} />)}
+      </Files>
+      <Meta>
+        <Buttons>
+          <Button>{isLiked ? <HeartFull /> : <HeartEmpty />}</Button>
+          <Button>
+            <Comment />
+          </Button>
+        </Buttons>
+        <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+        <Timestamp>{createdAt}</Timestamp>
+      </Meta>
+    </Post>
+  );
+};
