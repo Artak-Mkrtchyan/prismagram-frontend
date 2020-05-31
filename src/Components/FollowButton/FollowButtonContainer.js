8f74edcd4c65fb0d9864b27618774d7ccb9b232f
Artak <artak6297@gmail.com>
2020-05-31 23:41:01 +0400

    feat: added FOLLOW and UNFOLLOW functions

---
+++ src/Components/FollowButton/FollowButtonContainer.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useMutation } from "react-apollo-hooks";
+
+import FollowButtonPresenter from "./FollowButtonPresenter";
+import { FOLLOW, UNFOLLOW } from "./FollowButtonQueries";
+
+const FollowButtonContainer = ({ isFollowing, id }) => {
+  const [isFollowingS, setIsFollowing] = useState(isFollowing);
+  const [followMutation] = useMutation(FOLLOW);
+  const [unfollowMutation] = useMutation(UNFOLLOW);
+
+  const onClick = () => {
+    if (isFollowingS) {
+      setIsFollowing(false);
+      unfollowMutation({ variables: { id } });
+    } else {
+      setIsFollowing(true);
+      followMutation({ variables: { id } });
+    }
+  };
+
+  return <FollowButtonPresenter onClick={onClick} isFollowing={isFollowingS} />;
+};
+
+FollowButtonContainer.propTypes = {
+  isFollowing: PropTypes.bool.isRequired,
+  id: PropTypes.string.isRequired,
+};
+
+export default FollowButtonContainer;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,16 +2,68 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import Loader from "../../Components/Loader";
 import FatText from "../../Components/FatText";
+import UserCard from "../../Components/UserCard";
+
+const Section = styled.div`
+  margin-bottom: 50px;
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, 160px);
+  grid-template-rows: 160px;
+  grid-auto-rows: 160px;
+`;
 
 const Wrapper = styled.div`
   height: 50vh;
   text-align: center;
 `;
 
-const SearchPresenter = ({ searchTerm, loading }) => (
-  <Wrapper>{searchTerm === undefined && <FatText text={"Search for something"} />}</Wrapper>
-);
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+  if (searchTerm === undefined) {
+    return (
+      <Wrapper>
+        <FatText text='Search for something' />
+      </Wrapper>
+    );
+  } else if (loading) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else if (data && data.searchUser && data.searchPost) {
+    return (
+      <Wrapper>
+        <Section>
+          {data.searchUser.length === 0 ? (
+            <FatText text='No users found' />
+          ) : (
+            data.searchUser.map((user) => (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                username={user.username}
+                isFollowing={user.isFollowing}
+                url={user.avatar}
+                isSelf={user.isSelf}
+              />
+            ))
+          )}
+        </Section>
+        <Section>
+          {data.searchPost.length === 0 ? (
+            <FatText text='No post Found' />
+          ) : (
+            data.searchPost.map((post) => null)
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+  return;
+};
 
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
